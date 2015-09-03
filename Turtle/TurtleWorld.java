5eecfda8b6a60381348336cc759b6835e0981ca8
ericzaba <eazaba@NN123-HA-24.HS.sd203.org>
2015-09-03 15:00:04 -0500

    made circles and such.
    fun!

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -8,23 +8,42 @@ public class TurtleWorld
         Turtle turtle = new Turtle(turtleWorld);
         
         turtle.setPenColor(Color.CYAN);
-        
         turtle.penDown();
+        
         
         turtle.setPenWidth(1);
         
+        Turtle turtle1 = new Turtle(turtleWorld);
+        turtle1.setPenColor(Color.RED);
         
+        turtle1.penDown();
+        turtle1.setPenWidth(1);
         
-        for (int x = 0; x < 200; x++) {
-            System.out.println( x + Math.pow(x, 1.3)-100 );
-            if ( x<200 ){
-                turtle.moveTo( x, (int)Math.pow(x, 1.3 )-100  );
-            }
-            if ( x>30 ){
-                turtle.moveTo( x, (int)Math.pow( x, -1 ));
-            }
-         }
-    }
-    
-                
+        Turtle turtle2 = new Turtle(turtleWorld);
+        turtle2.setPenColor(Color.MAGENTA);
+        turtle2.penDown();
+        turtle2.setPenWidth(1);
+        
+        for (int x=10; x<200; x++){
+            
+            turtle.forward(x);
+           
+            turtle.turn(45);
+            
+        }
+        for (int y=10; y<200; y++){
+            
+            turtle1.forward(y);
+            turtle1.turn(-90);
+       }
+       for (int z=10; z<200; z++){
+           turtle2.forward(z);
+           turtle2.turn(45);
+           
+          
+        
+        }
+        
+       
+    }     
 }
