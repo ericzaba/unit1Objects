46e9b3764346800eac1925aa3a44d6abc932fa98
ericzaba <eazaba@NN123-HA-24.HS.sd203.org>
2015-09-03 15:10:11 -0500

    asdfasdf

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -5,45 +5,42 @@ public class TurtleWorld
     public static void main(String args[])
     {
         World turtleWorld = new World();
-        Turtle turtle = new Turtle(turtleWorld);
         
+        Turtle turtle = new Turtle(turtleWorld);
         turtle.setPenColor(Color.CYAN);
         turtle.penDown();
-        
-        
         turtle.setPenWidth(1);
         
-        Turtle turtle1 = new Turtle(turtleWorld);
-        turtle1.setPenColor(Color.RED);
+        Turtle turtler = new Turtle(turtleWorld);
+        turtler.setPenColor(Color.RED);
+        turtler.penDown();
+        turtler.setPenWidth(1);
         
-        turtle1.penDown();
-        turtle1.setPenWidth(1);
+        Turtle turtlem = new Turtle(turtleWorld);
+        turtlem.setPenColor(Color.MAGENTA);
+        turtlem.penDown();
+        turtlem.setPenWidth(1);
         
-        Turtle turtle2 = new Turtle(turtleWorld);
-        turtle2.setPenColor(Color.MAGENTA);
-        turtle2.penDown();
-        turtle2.setPenWidth(1);
-        
-        for (int x=10; x<200; x++){
+        for ( int x=10; x<200; x++ ){
             
             turtle.forward(x);
            
-            turtle.turn(45);
+            turtle.turn(120);
             
         }
-        for (int y=10; y<200; y++){
+        for ( int y=10; y<200; y++ ){
             
-            turtle1.forward(y);
-            turtle1.turn(-90);
+            turtler.forward(y);
+            turtler.turn(-90);
        }
-       for (int z=10; z<200; z++){
-           turtle2.forward(z);
-           turtle2.turn(45);
+       for ( int z=10; z<200; z++ ){
+           turtlem.forward(z);
+           turtlem.turn(-120);
            
           
         
         }
-        
+       
        
     }     
 }
