a550a7dc8c275ea2c12cd7db0d9dbc96c0cb0792
ericzaba <eazaba@NN123-HA-24.HS.sd203.org>
2015-09-04 14:37:54 -0500

    Fniished and added 4th turtle

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -21,6 +21,16 @@ public class TurtleWorld
         turtlem.penDown();
         turtlem.setPenWidth(1);
         
+        Turtle turtleg= new Turtle(turtleWorld);
+        turtleg.setPenColor(Color.GREEN);
+        turtleg.turn(180);
+        turtleg.setPenWidth(1);
+        
+        Turtle turtley = new Turtle(turtleWorld);
+        turtley.setColor(Color.YELLOW);
+       
+        turtley.setPenWidth(1);
+        
         for ( int x=10; x<200; x++ ){
             
             turtle.forward(x);
@@ -28,19 +38,27 @@ public class TurtleWorld
             turtle.turn(120);
             
         }
-        for ( int y=10; y<200; y++ ){
+         for ( int y=10; y<200; y++ ){
             
             turtler.forward(y);
             turtler.turn(-90);
-       }
-       for ( int z=10; z<200; z++ ){
+        }
+        for ( int z=10; z<200; z++ ){
            turtlem.forward(z);
            turtlem.turn(-120);
            
           
         
         }
-       
-       
+       for ( int a=10; a<200; a++){
+           turtleg.forward(a);
+           turtleg.turn(-120);
+           
+        }
+        for ( int b=10; b<200; b++){
+            turtley.forward(b);
+            turtley.turn(-120);
+        }
+
     }     
 }
